1749dbb927746a9c67cf131fec0443732040ef49
Shin seongsu <tlstjdtn321@naver.com>
2021-08-29 17:57:53 +0900

    중복문자제거

---
+++ 김태원 - 자바스크립트 문제풀이/section1/16-중복문자제거.js
@@ -0,0 +1,27 @@
+function solution(str) {
+    // 배열 내장 메서드 이용하기
+    //   return str
+    //     .split('')
+    //     .reduce((result, s) => {
+    //       if (!result.includes(s)) result.push(s);
+    //       return result;
+    //     }, [])
+    //     .join('');
+
+    // Set 개념 이용하기
+    // const set = new Set();
+    // str.split('').forEach((str) => set.add(str));
+    // const result = [];
+    // for (const value of set) {
+    //     result.push(value);
+    // }
+    // return result.join('');
+
+    let answer = '';
+    for (let i = 0; i < str.length; i++) {
+        if (str.indexOf(str[i] === i)) answer += str[i];
+    }
+    return answer;
+}
+
+console.log(solution('ksekkset'));
