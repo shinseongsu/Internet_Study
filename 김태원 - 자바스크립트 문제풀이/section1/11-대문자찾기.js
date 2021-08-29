371e6ce2837562af9e9aebb1de1b14193dbd2b9f
Shin seongsu <tlstjdtn321@naver.com>
2021-08-29 17:37:43 +0900

    Delete 김태원 - 자바스크립트 문제풀이/section1 directory

--- 김태원 - 자바스크립트 문제풀이/section1/11-대문자찾기.js
+++
@@ -1,9 +0,0 @@
-function solution(arg) {
-    let answer = 0;
-    for(let x of arg) {
-        if (x === x.toUpperCase()) answer++;
-    }
-    return answer;
-}
-
-console.log(solution('KoreaTimeGood'));
