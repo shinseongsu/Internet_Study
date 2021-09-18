e5e8cb800c9c5d16c18d7c60480eec3c227ef39d
Shin seongsu <tlstjdtn321@naver.com>
2021-09-18 23:15:48 +0900

    Annotation

---
+++ 백기선-더자바/src/main/java/com/example/chapter7/App.java
@@ -0,0 +1,18 @@
+package com.example.chapter7;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) {
+        List<@Egg String> naems = Arrays.asList("junhan");
+    }
+
+    static class FeelsLikeChicken<@Egg T> {
+        public static <@Egg C> void print(@Egg C c) {
+            System.out.println(c);
+        }
+    }
+
+}
---
+++ 백기선-더자바/src/main/java/com/example/chapter7/Chicken.java
@@ -0,0 +1,10 @@
+package com.example.chapter7;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+@Repeatable(ChickenContainer.class)
+public @interface Chicken {
+    String value();
+}
