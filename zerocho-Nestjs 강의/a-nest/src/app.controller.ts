f2222caaff509d6d33b84e2c022f2d5dfc877b8b
Shin seongsu <tlstjdtn321@naver.com>
2021-09-29 22:59:08 +0900

    GET, POST 추가

---
+++ zerocho-Nestjs 강의/a-nest/src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('abc')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('user') // GET /abc/user
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('user') // POST /abc/user
+  postHello(): string {
+    return this.appService.getHello();
+  }
+}
