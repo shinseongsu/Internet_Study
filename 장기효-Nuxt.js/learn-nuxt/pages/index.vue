badfac377cd42f5c54abc0d509b17aeb5d6c7e53
Shin seongsu <tlstjdtn321@naver.com>
2021-10-24 17:29:34 +0900

    json 파싱 후, asyncData 사용 해보기

--- 장기효-Nuxt.js/learn-nuxt/pages/index.vue
+++ 장기효-Nuxt.js/learn-nuxt/pages/index.vue
@@ -1,11 +1,70 @@
 <template>
-  <div>
-    <p>넉스트 시작하기</p>
+  <div class="app">
+    <main>
+      <div>
+        <input type="text" />
+      </div>
+      <ul>
+        <li v-for="product in products" :key="product.id" class="item flex">
+          <img
+            class="product-image"
+            :src="product.imageUrl"
+            alt="product.name"
+          />
+          <p>{{ product.name }}</p>
+          <span>{{ product.price }}</span>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
-export default {}
+import axios from 'axios'
+
+export default {
+  async asyncData() {
+    const response = await axios.get('http://localhost:3000/products')
+    console.log(response)
+    const products = response.data.map((item) => ({
+      ...item,
+      imageUrl: `${item.imageUrl}?random=${Math.random()}`,
+    }))
+    return { products }
+  },
+}
 </script>
 
-<style></style>
+<style scoped>
+.flex {
+  display: flex;
+  justify-content: center;
+}
+.item {
+  display: inline-block;
+  width: 400px;
+  height: 300px;
+  text-align: center;
+  margin: 0 0.5rem;
+  cursor: pointer;
+}
+.product-image {
+  width: 400px;
+  height: 250px;
+}
+.app {
+  position: relative;
+}
+.cart-wrapper {
+  position: sticky;
+  float: right;
+  bottom: 50px;
+  right: 50px;
+}
+.cart-wrapper .btn {
+  display: inline-block;
+  height: 40px;
+  font-size: 1rem;
+  font-weight: 500;
+}
+</style>
