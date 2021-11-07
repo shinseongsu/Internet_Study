2ff5339fd632521b4fbc07442a0e5255669f828c
Shin seongsu <tlstjdtn321@naver.com>
2021-11-07 23:44:14 +0900

    카드 구매하기

---
+++ 코드플러스-알고리즘 기초-1/dp5/src/dp5/Main.java
@@ -0,0 +1,47 @@
+package dp5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer tokenizer;
+
+        try {
+            int N = Integer.parseInt(br.readLine());
+            tokenizer = new StringTokenizer(br.readLine());
+            int[] price = new int[N + 1];
+            int[] d = new int[N + 1];
+            for(int i = 1; i < N + 1; i++) {
+                price[i] = Integer.parseInt(tokenizer.nextToken());
+            }
+            d[0] = 0;
+            d[1] = price[1];
+
+            for(int i = 2; i <= N; i++) {
+                for(int j = 1; j <= i; j++) {
+                    if(d[i] < d[i - j] + price[j]) {
+                        d[i] = d[i - j] + price[j];
+                    }
+                }
+            }
+
+            bw.append(d[N] + "");
+            bw.close();
+            br.close();
+
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
