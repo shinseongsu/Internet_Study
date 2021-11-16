c2346e7307cf5d75c1ac9f79f64b455f463b665f
Shin seongsu <tlstjdtn321@naver.com>
2021-11-16 23:56:53 +0900

    테스트 주도 개발 해보기

---
+++ 김정환 - 테스트주도개발 nodeAPI/first-api/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var users = [
+    { id: 1, name: 'alice' },
+    { id: 2, name: 'bak' },
+    { id: 3, name: 'Chris' },
+];
+
+app.use(morgan('dev'));
+
+app.get('/users', function (req, res) {
+    res.json(users);
+});
+
+app.listen(3000, function () {
+    console.log('Exmple app listening on port 3000!');
+});
---
+++ 김정환 - 테스트주도개발 nodeAPI/first-api/utils.spec.js
@@ -0,0 +1,7 @@
+const utils = require('./utils');
+
+describe('utils.js 모듈의 capitalize() 함수는', () => {
+    it('문자열의 첫번째 문자를 대문자로 변환한다.', () => {
+        // ...
+    });
+});
