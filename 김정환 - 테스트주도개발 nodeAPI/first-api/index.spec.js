230fc52570bf9b4c6993cfa7a64b6dbb7f85fc68
Shin seongsu <tlstjdtn321@naver.com>
2021-11-17 08:56:00 +0900

    superTest로 api 간단 테스트 하기

--- 김정환 - 테스트주도개발 nodeAPI/first-api/index.spec.js
+++ 김정환 - 테스트주도개발 nodeAPI/first-api/index.spec.js
@@ -1,13 +1,44 @@
-const app = require('./index');
 const request = require('supertest');
+const should = require('should');
+const app = require('./index');
 
 describe('GET /users는', () => {
-    it('...', (done) => {
+    describe('성공시', () => {
+        it('유저 객체를 담은 배열로 응답한 ', (done) => {
+            request(app)
+                .get('/users')
+                .end((err, res) => {
+                    res.body.should.be.instanceOf(Array);
+                    done();
+                });
+        });
+    });
+
+    it('최대 limit 갯수만큼 응답한다. ', (done) => {
         request(app)
-            .get('/users')
+            .get('/users?limit=2')
             .end((err, res) => {
-                console.log(res.body);
+                res.body.should.have.length(2);
                 done();
             });
+    });
+
+    describe('실패시', () => {
+        it('limit의 숫자들이 아니면 400을 응답한다.', (done) => {
+            request(app).get('/users?limit=two').expect(400).end(done);
+        });
+    });
+});
+
+describe('GET /users/1는 ', () => {
+    describe('성공시', () => {
+        it('id가 1인 유저 객체를 반환한다.', (done) => {
+            request(app)
+                .get('/users/1')
+                .end((err, res) => {
+                    res.body.should.have.property('id', 1);
+                    done();
+                });
+        });
     });
 });
