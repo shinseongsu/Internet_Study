c52869a8e4b56b1cdd091ebdd6f41e6a280cd0d1
Shin seongsu <tlstjdtn321@naver.com>
2021-11-24 22:26:02 +0900

    백기선 웹 MVC 맛보기

---
+++ 백기선 - 웹 MVC/스프링 MVC 동작원리/src/main/java/com/example/mvc/MvcSpringApplication.java
@@ -0,0 +1,11 @@
+package com.example.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MvcSpringApplication.class, args);
+    }
+}
