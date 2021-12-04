64496b7238211e18de4b7b74138d434257c87c60
Shin seongsu <tlstjdtn321@naver.com>
2021-12-04 22:40:59 +0900

    기본 베이스

---
+++ 유데미-WebRTC/my-app/src/IntroductionPage/IntroductionPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IntroductionPage = () => {
+  return <div>Hello here is the introduction page</div>;
+};
+
+export default IntroductionPage;
---
+++ 유데미-WebRTC/my-app/src/JoinRoomPage/JoinRoomPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const JoinRoomPage = () => {
+  return <div>Hello here is the JoinRoomPage page</div>;
+};
+
+export default JoinRoomPage;
