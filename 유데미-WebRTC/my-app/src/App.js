7b3c070505a1c644f42479faf3ee880fcea4fe53
Shin seongsu <tlstjdtn321@naver.com>
2021-12-06 23:17:34 +0900

    css 추가

---
+++ 유데미-WebRTC/my-app/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import JoinRoomPage from "./JoinRoomPage/JoinRoomPage";
+import RoomPage from "./RoomPage/RoomPage";
+import IntroductionPage from "./IntroductionPage/IntroductionPage";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/join-room">
+          <JoinRoomPage />
+        </Route>
+        <Route path="/room">
+          <RoomPage />
+        </Route>
+        <Route path="/">
+          <IntroductionPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ 유데미-WebRTC/my-app/src/RoomPage/RoomPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomPage = () => {
+  return <div>Hello here is the RoomPage page</div>;
+};
+
+export default RoomPage;
