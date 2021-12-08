f8adead0fde43ce6479aaa5f61f92329c83547e9
Shin seongsu <tlstjdtn321@naver.com>
2021-12-08 22:44:30 +0900

    title 변경

--- 유데미-WebRTC/my-app/src/JoinRoomPage/JoinRoomPage.js
+++ 유데미-WebRTC/my-app/src/JoinRoomPage/JoinRoomPage.js
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { connect } from "react-redux";
+import { setIsRoomHost } from "../store/actions";
+import JoinRoomTitle from "./JoinRoomTitle";
 
-import "../JoinRoomPage.css";
+import "./JoinRoomPage.css";
 
-const JoinRoomPage = () => {
+const JoinRoomPage = (props) => {
+  const { setIsRoomHostAction, isRoomHost } = props;
+
+  const search = useLocation().search;
+
+  useEffect(() => {
+    const isRoomHost = new URLSearchParams(search).get("host");
+    if (isRoomHost) {
+      setIsRoomHostAction(true);
+    }
+  }, []);
+
   return (
     <div className="join_room_page_container">
-      <div className="join_room_page_pane1"></div>
+      <div className="join_room_page_panel">
+        <JoinRoomTitle isRoomHost={isRoomHost} />
+      </div>
     </div>
   );
 };
 
-export default JoinRoomPage;
+const mapStoreStateToProps = (state) => {
+  return {
+    ...state,
+  };
+};
+
+const mapActionsToProps = (dispatch) => {
+  return {
+    setIsRoomHostAction: (isRoomHost) => dispatch(setIsRoomHost(isRoomHost)),
+  };
+};
+
+export default connect(mapStoreStateToProps, mapActionsToProps)(JoinRoomPage);
