583564ec57aa7d2c7405827348da2da91eb3275e
Shin seongsu <tlstjdtn321@naver.com>
2021-12-10 23:28:23 +0900

    멀티 쓰레드 공부

---
+++ 유데미-멀티쓰레드/chapter2/src/main/java/thread/cration/example/Main.java
@@ -0,0 +1,16 @@
+package thread.cration.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Thread thread = new NewThread();
+
+        thread.start();
+    }
+
+    private static class NewThread extends Thread {
+        @Override
+        public void run() {
+            System.out.println("Hello from " + this.getName());
+        }
+    }
+}
