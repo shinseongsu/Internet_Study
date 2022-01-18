27c811a7810f22fe223ce570975b649cb536cde6
Shin seongsu <tlstjdtn321@naver.com>
2022-01-18 23:33:12 +0900

    thread join 사용

---
+++ 유데미-멀티쓰레드/chapter3/src/main/java/thread/interrupt/Main.java
@@ -0,0 +1,26 @@
+package thread.interrupt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new BlockingTask());
+
+        thread.start();
+
+        thread.interrupt();
+    }
+
+    private static class BlockingTask implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(50000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
