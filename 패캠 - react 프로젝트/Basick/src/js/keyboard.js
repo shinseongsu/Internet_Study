47bf99f96ae19224eb56ee02bb83339a2543ce2f
Shin seongsu <tlstjdtn321@naver.com>
2022-01-27 23:30:30 +0900

    keyboard keyup 효과 추가

--- 패캠 - react 프로젝트/Basick/src/js/keyboard.js
+++ 패캠 - react 프로젝트/Basick/src/js/keyboard.js
@@ -1,21 +1,43 @@
 export class Keyboard {
   #swichEl;
+  #fontSelectEl;
+  #containerEl;
+  #keyboardEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
   }
 
   #assignElement() {
-    this.#swichEl = document.getElementById("switch");
+    this.#containerEl = document.getElementById("container");
+    this.#swichEl = this.#containerEl.querySelector("#switch");
+    this.#fontSelectEl = this.#containerEl.querySelector("#font");
+    this.#keyboardEl = this.#containerEl.querySelector("#keyboard");
   }
 
   #addEvent() {
-    this.#swichEl.addEventListener("change", (event) => {
-      document.documentElement.setAttribute(
-        "theme",
-        event.target.checked ? "dark-mode" : ""
-      );
-      console.log(event.target.checked);
+    this.#swichEl.addEventListener("change", this.#onChangeTheme);
+    this.#fontSelectEl.addEventListener("change", this.#onChangeFont);
+    document.addEventListener("keydown", (event) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        .classList.add("active");
     });
+    document.addEventListener("keyup", (event) => {
+      this.#keyboardEl
+        .querySelector(`[data-code=${event.code}]`)
+        .classList.remove("active");
+    });
+  }
+
+  #onChangeTheme(event) {
+    document.documentElement.setAttribute(
+      "theme",
+      event.target.checked ? "dark-mode" : ""
+    );
+  }
+
+  #onChangeFont(event) {
+    document.body.style.fontFamily = event.target.value;
   }
 }
