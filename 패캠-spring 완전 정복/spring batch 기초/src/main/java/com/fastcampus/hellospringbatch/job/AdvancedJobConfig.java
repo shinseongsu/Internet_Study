644ad58e51518e45199b65070c1e28ec0c439ebf
Shin seongsu <tlstjdtn321@naver.com>
2022-02-22 22:57:32 +0900

    spring batch itemListener

---
+++ 패캠-spring 완전 정복/spring batch 기초/src/main/java/com/fastcampus/hellospringbatch/job/AdvancedJobConfig.java
@@ -0,0 +1,78 @@
+package com.fastcampus.hellospringbatch.job;
+
+import com.fastcampus.hellospringbatch.job.validator.LocalDateParameterValidator;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+
+@Configuration
+@AllArgsConstructor
+@Slf4j
+public class AdvancedJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job advancedJob(JobExecutionListener jobExecutionListener,
+                           Step advancedStep) {
+        return jobBuilderFactory.get("advancedJob")
+                .incrementer(new RunIdIncrementer())
+                .validator(new LocalDateParameterValidator("targetDate"))
+                .listener(jobExecutionListener)
+                .start(advancedStep)
+                .build();
+    }
+
+    @JobScope
+    @Bean
+    public JobExecutionListener jobExecutionListener() {
+        return new JobExecutionListener() {
+            @Override
+            public void beforeJob(JobExecution jobExecution) {
+                log.info("[JobExecutionListener#beforeJob] jobExecution is " + jobExecution.getStatus());
+            }
+
+            @Override
+            public void afterJob(JobExecution jobExecution) {
+                if (jobExecution.getStatus() == BatchStatus.FAILED) {
+                    log.error("[JobExecutionListener#afterJob] jobExecution is FAILED!!! RECOVER ASAP");
+                }
+            }
+        };
+    }
+
+
+    @JobScope
+    @Bean
+    public Step advancedStep(Tasklet advancedTasklet) {
+        return stepBuilderFactory.get("advanceStep")
+                .tasklet(advancedTasklet)
+                .build();
+    }
+
+    @StepScope
+    @Bean
+    public Tasklet advancedTasklet(@Value("#{jobParameters['targetDate']}") String targetDate) {
+        return (contribution, chunkContext) -> {
+            log.info("[AdvancedTasklet] jobParameter - targetDate = " + targetDate);
+            LocalDate executionDate = LocalDate.parse(targetDate);
+            log.info("[AdvancedTasklet] excuted advancedTasklet");
+            throw new RuntimeException("ERROR!!!");
+    //        return RepeatStatus.FINISHED;
+        };
+    }
+
+}
