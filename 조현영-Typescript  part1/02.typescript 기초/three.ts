6deb4e84d43250229b938a4e6e6104280c53150a
Shin seongsu <tlstjdtn321@naver.com>
2023-02-20 22:52:41 +0900

    wrapper 클래스로 덮으면 에러가 발생하기 쉽다.

---
+++ 조현영-Typescript  part1/02.typescript 기초/three.ts
@@ -0,0 +1,8 @@
+const first: string = "hello";
+const second: String = "hell";
+// 대문자 쓰지 말라 - wrapper 객체로 인식
+
+function addFunction(a1: string, b1: string) {}
+addFunction(first, second);
+
+new String();
