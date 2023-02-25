9a505d267f479a63777c1c7b4c29c9bc270a95ba
Shin seongsu <tlstjdtn321@naver.com>
2023-02-25 17:05:29 +0900

    :sparkles: 마블 다이어그램 예제 코드 추가

---
+++ kevin-rxjava/src/main/java/com/example/chapter02/MarbleExample02.java
@@ -0,0 +1,11 @@
+package com.example.chapter02;
+
+import io.reactivex.Observable;
+
+public class MarbleExample02 {
+    public static void main(String[] args) {
+        Observable<Integer> observable = Observable.just(2, 25, 30, 15, 6);
+
+        observable.subscribe(num -> System.out.println(num));
+    }
+}
